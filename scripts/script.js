f7dff316f08c0aaf714664f40197c7a1d2795738
mattg1243 <mattgallucci97@gmail.com>
2021-08-18 12:49:04 -0700

    the pixels are staying filled finally, other various clean up

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,15 @@
 const body = document.querySelector('body')
 const sketchCont = document.querySelector('#sketchcont');
-const sketchCell = document.querySelector('.sketchcell');
+const sketchCell = document.querySelector('#cellOrigin');
 
+
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+function clearGrid() {
+    allCells.style.setProperty('background-color', 'white');
+}
 
 function setGrid(rows, columns) {
 
@@ -9,12 +17,19 @@ function setGrid(rows, columns) {
     sketchCont.style.setProperty('--grid-columns', columns);
     
     for (let i = 0; i < (rows * columns) ; i++) {
-        sketchCont.appendChild(sketchCell);
-        let newcell = document.createElement('div');
-        newcell.classList.add('sketchcell');
-
+        const newcell = document.createElement('div');
+        newcell.setAttribute('class', 'sketchCell');
+        newcell.addEventListener('mouseover', changeColor)
         sketchCont.appendChild(newcell);
     }
+}
+
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+function clearGrid() {
+    allCells.style.setProperty('background-color', 'white');
 }
 
 setGrid(32, 32);
