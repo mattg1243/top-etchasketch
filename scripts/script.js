a4e98ec175ecad0bbd076f0babcd8b623914e54c
mattg1243 <mattgallucci97@gmail.com>
2021-08-17 17:54:08 -0700

    got the cells to appear black when hovered over, need to make the container always be filled and every cell the same square size

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,20 @@
 const body = document.querySelector('body')
-const sketchCont = document.querySelector('.sketchcont');
+const sketchCont = document.querySelector('#sketchcont');
 const sketchCell = document.querySelector('.sketchcell');
 
 
-function defaultGrid(dimensions) {
+function setGrid(rows, columns) {
 
-    for (let i = 0; i < dimensions ; i++) {
+    sketchCont.style.setProperty('--grid-rows', rows);
+    sketchCont.style.setProperty('--grid-rows', columns);
+    
+    for (let i = 0; i < (rows * columns) ; i++) {
         sketchCont.appendChild(sketchCell);
-    }
+        let newcell = document.createElement('div');
+        newcell.classList.add('sketchcell');
 
+        sketchCont.appendChild(newcell);
+    }
 }
 
-defaultGrid(16);
+setGrid(16, 16);
