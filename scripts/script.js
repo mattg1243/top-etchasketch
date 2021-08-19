4906cc0e08bd52cc74a16b873abfb884cdbdb838
mattg1243 <mattgallucci97@gmail.com>
2021-08-18 17:25:45 -0700

    removed everything and made the reset button "shake", added material button and now it works

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
+const DEFAULT_SIZE = 16;
+
 const body = document.querySelector('body')
 const sketchCont = document.querySelector('#sketchcont');
 const sketchCell = document.querySelector('#cellOrigin');
 const allCells = document.querySelectorAll('.sketchcell')
-const resetButton = document.querySelector('#reset')
+const resetButton = document.querySelector('#reset');
+const randomColorButton = document.querySelector('#rancolor');
 
 resetButton.onclick = () => reset();
 
@@ -12,18 +15,16 @@ function draw(e) {
     e.target.style.backgroundColor = drawingColor;
 }
 
-function reset() {
-    let newsize = prompt('How big would you like your new grid?');
-    setGrid(newsize, newsize);
-    clearGrid();
-}
-
 function changeColor(newColor) {
     drawingColor = newColor;
 }
 
+function randomColor() {
+    drawingColor = Math.floor(Math.random()*16777215).toString(16);
+}
+
 function clearGrid() {
-    allCells.forEach(cell => cell.style.setProperty('background-color', 'white'));
+    sketchCont.innerHTML = '';
 }
 
 function setGrid(rows, columns) {
@@ -33,11 +34,16 @@ function setGrid(rows, columns) {
     
     for (let i = 0; i < (rows * columns) ; i++) {
         const newcell = document.createElement('div');
-        newcell.setAttribute('class', 'sketchCell');
+        //newcell.setAttribute('class', 'sketchCell');
         newcell.addEventListener('mouseover', draw)
         sketchCont.appendChild(newcell);
     }
 }
 
+function reset() {
+    let newsize = prompt('How big would you like your new grid?');
+    clearGrid();
+    setGrid(newsize, newsize);
+}
 
 setGrid(32, 32);
